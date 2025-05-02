00fc95b32c09873ebe2cecd3b1ac19e09d2d0b7d
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2025-05-02 22:42:26 +0200

    oops

--- back/config/config.php
+++ back/config/config.php
@@ -2,7 +2,7 @@
 // Configuration de la base de données
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'Respons11');
 define('DB_NAME', 'gestion_notes');
 
 // Configuration de la session
