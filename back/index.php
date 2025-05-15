3800a39ebf72604c88c3f311ea9e40b0522200e1
MrRobboy <mathis.ngoo@gmail.com>
2025-05-15 14:20:59 +0200

    wip

--- back/index.php
+++ back/index.php
@@ -1,21 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<configuration>
-    <system.webServer>
-        <defaultDocument>
-            <files>
-                <clear />
-                <add value="info.php" />
-                <add value="index.php" />
-                <add value="default.php" />
-            </files>
-        </defaultDocument>
-        <rewrite>
-            <rules>
-                <rule name="Redirect root to info.php" stopProcessing="true">
-                    <match url="^$" />
-                    <action type="Redirect" url="info.php" />
-                </rule>
-            </rules>
-        </rewrite>
-    </system.webServer>
-</configuration>
+<?php
+// Redirection vers info.php
+header('Location: info.php');
+exit;
